8900bd3530236160ccf78d752509904525c368bf
hj <1959647336@qq.com>
2020-04-18 22:09:53 +0800

    4.18

--- python_code/Untitled-1.py
+++ python_code/Untitled-1.py
@@ -1,12 +1,3 @@
-import time
-import itertools
-
-time_start=time.time()
-sum2=0 
-a=[1,2,3,4] 
-for i in itertools.permutations(a,3): 
-    print(i) 
-sum2+=1 
-time_end=time.time()
-print(sum2)
-print('time cost',time_end-time_start,'s')
+m = [1,2,3,4,59,6]
+n = 2
+print(m[6-n:])
