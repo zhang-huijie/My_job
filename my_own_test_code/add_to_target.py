b3ac4d327a982c201a76b195e84f8523697c3674
hj <1959647336@qq.com>
2020-04-30 22:35:52 +0800

    Revision

--- my_own_test_code/add_to_target.py
+++ my_own_test_code/add_to_target.py
@@ -13,5 +13,20 @@ for i in range(0,len(nums)-1):
                 tag[i],tag[j] = 0,0
             else:
                 continue
-
-
+'''
+#leecode上面题目解决方法，空间开销代替时间开销
+def solution(nums,target):
+	#新建立一个空字典用来保存数值及其在列表中对应的索引
+	dict1 = {}
+	#遍历一遍列表对应的时间复杂度为O(n)
+        for i in range(0, len(nums)):
+            #相减得到另一个数值
+            num = target - nums[i]
+            #如果另一个数值不在字典中，则将第一个数值及其的索引报错在字典中
+            #因为在字典中查找的时间复杂度为O(1)，因此总时间复杂度为O(n)
+            if num not in dict1:
+                dict1[nums[i]] = i
+            #如果在字典中则返回
+            else:
+                return [dict1[num], i]
+'''
