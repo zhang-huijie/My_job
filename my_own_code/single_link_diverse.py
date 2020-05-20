5c01074e8d36204b7455ee6c01aa49f05728e49d
1959647336@qq.com <1959647336@qq.com>
2020-05-20 17:14:57 +0800

    tence

--- my_own_code/single_link_diverse.py
+++ my_own_code/single_link_diverse.py
@@ -16,6 +16,25 @@ def rev(link):
         pre = cur
         cur = temp
     return pre
+
+#/////////////////////////////////////////////////
+#方法二
+def reverseList1(self, head):
+    """
+    leetcode官方解法，思路和上一个解法大同小异，重点是在第1个节点前构造一个虚拟节点
+    """
+    if head is None or head.next is None:  # 兼容leetcode特殊用例，链表为空或只有1个节点
+        return head
+    pre_node = None
+    current_node = head
+    while current_node is not None:
+        next_node = current_node.next
+        current_node.next = pre_node
+        pre_node = current_node
+        current_node = next_node
+    return pre_node
+#/////////////////////////////////////////////////////////
+
 if __name__ == '__main__':
     link = Node(1, Node(2, Node(3, Node(4, Node(5, Node(6, Node(7, Node(8, Node(9)))))))))
     root = rev(link)
