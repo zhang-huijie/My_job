2220fcea1ef66400d9e83daa67e2f23673837ba4
1959647336@qq.com <1959647336@qq.com>
2020-05-20 17:12:53 +0800

    tence

--- my_own_code/qsort1.py
+++ my_own_code/qsort1.py
@@ -21,6 +21,20 @@ def qsort(nums, l, r):
 		qsort(nums, l, mid-1)
 		qsort(nums, mid+1, r)
 
+#//////////////////////////////////////////////////////////////
+# 空间开销比较大的另一种方式
+def quick_sort(seq):
+    if(len(seq) < 2):
+        return seq
+    else:
+        base = seq[0]
+        left = [elem for elem in seq[1:] if elem < base]
+        right = [elem for elem in seq[1:] if elem > base]
+        return quick_sort(left) + [base] + quick_sort(right)
+#//////////////////////////////////////////////////////////////
+
+
+
 if __name__ == "__main__":
 	nums = [4,3,2,5,1]
 	qsort(nums, 0, len(nums)-1)
