190a44ff2a14c0972da1c8397074beac4d7ccb47
1959647336@qq.com <1959647336@qq.com>
2020-05-20 17:49:30 +0800

    tence

--- my_own_code/longestCommonPrefix.py
+++ my_own_code/longestCommonPrefix.py
@@ -1,12 +1,24 @@
-#实现最长公共前缀
+#实现最长公共前缀leecode-14
 def subString(strs):
     result=strs[0]
     for i in range(1,len(strs)):
         while (strs[i].startswith(result)==False):
             result=result[0:len(result)-1]
-            if len(result)==0:
+            if len(result) == 0:
                 return "无公共前缀"
     return result
+
+#///////////////////////////////
+#解法二
+def longestCommonPrefix( strs):
+    if not strs: return ""
+    s1 = min(strs)
+    s2 = max(strs)
+    for i, x in enumerate(s1):
+        if x != s2[i]:
+            return s2[:i]
+    return s1
+#/////////////////////////////////
 if __name__=='__main__':
     result=subString(['abc','ab','abn'])
     print (result)
