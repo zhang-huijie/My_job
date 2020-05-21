98b792d95daa0164c4d2f99a52b27eac4ac64cb8
1959647336@qq.com <1959647336@qq.com>
2020-05-21 21:39:49 +0800

    tence

--- my_own_code/lastRemaining.py
+++ my_own_code/lastRemaining.py
@@ -1,10 +1,14 @@
 #剑指offer-面试题62
-#采用倒推的方法
+#采用倒推的方法,约瑟夫环
 class Solution:
     def lastRemaining(self, n: int, m: int) -> int:
         index_tmp = 0
         for i in range(2,n+1):
             index_tmp = (index_tmp + m) % i
         return index_tmp
-    
+
+if __name__ == '__main__':
+    a = Solution()
+    print(a.lastRemaining(5,3))
+
 
